0781ab6d81959bd5ddc9d2b56e26c0f5af4d01f3
YxZo <v.lopezlopez@student.helmo.be>
2023-03-29 15:05:07 +0200

    debut bd pour utilisateur

---
+++ libs/repository/BdConnect.inc.php
@@ -0,0 +1,23 @@
+<?php
+require 'BdConfig.inc.php';
+
+use PDO;
+use PDOException;
+class BdConnect
+{
+    public static function connect2db($base, &$message){
+        try {
+            $link = new PDO('mysql:host=' . MYHOST . ';dbname=' . $base . ';charset=UTF8', MYUSER, MYPASS);
+            $link->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
+            $link->exec("set names utf8");
+            $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            $message .= $e->getMessage().'<br>';
+            $link = false;
+        }
+        return $link;
+    }
+    public static function disconnect (&$link) {
+        $link = null;
+    }
+}
