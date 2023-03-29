c5504e6148d0b8c38a5d31631898c0a18cad3a90
YxZo <v.lopezlopez@student.helmo.be>
2023-03-29 15:05:07 +0200

    debut bd pour utilisateur

--- libs/models/User.php
+++ libs/models/User.php
@@ -10,4 +10,24 @@ class User
     private $email;
     private $idManager;
 
+    /**
+     * User constructor.
+     * @param $id int l'identifiant de l'utilisateur
+     * @param $name string le nom de l'utilisateur
+     * @param $firstName string le prenom de l'utilisateur
+     * @param $email string l'email de l'utilisateur
+     * @param $idManager int l'identifiant du manager de l'utilisateur (peut être null)
+     */
+    public function __construct($id, $name, $firstName, $email, $idManager)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->firstName = $firstName;
+        $this->email = $email;
+        $this->idManager = $idManager;
+    }
+
+
+
+
 }
---
+++ libs/repository/BdTable.inc.php
@@ -0,0 +1,8 @@
+<?php
+const TABLE_USER = 'user';
+const TABLE_TRAINING = 'training';
+const TABLE_PARTICIPATION = 'participation';
+const TABLE_TRAINER = 'trainer';
+const TABLE_FONCTION = 'fonction';
+const TABLE_FONCTION_TRAINING = 'manager';
+const TABLE_FONCTION_USER = 'manager';
