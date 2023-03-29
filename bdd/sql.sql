9ed29c1ae493db93274c4b6f07cbea4a104a4f6b
Tom CAUFRIER <t.caufrier@student.helmo.be>
2023-03-29 16:54:11 +0200

    Update sql.sql

--- bdd/sql.sql
+++ bdd/sql.sql
@@ -26,8 +26,7 @@ CREATE TABLE `Formation` (
   `date_limite` date NOT NULL,
   `confirmation` bool NOT NULL,
   `actif` bool NOT NULL,
-  `date_certificat_limite` date NOT NULL,
-  `id_requis` int(11)
+  `date_certificat_limite` date NOT NULL
 );
 
 CREATE TABLE `Formateur` (
@@ -44,15 +43,20 @@ CREATE TABLE `Participe` (
   `id_participe` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `id_utilisateur` int(11) NOT NULL,
   `id_formation` int(11) NOT NULL,
-  `statut` SET("EN ATTENTE", "EN FORMATION","FORMATION TERMINEE","DEPOT FAIT", "VALIDEE") NOT NULL,
+  `statut` SET('EN ATTENTE', 'EN FORMATION','FORMATION TERMINEE','DEPOT FAIT', 'VALIDEE') NOT NULL,
   `lien_fichier` varchar(500)
 );
 
 CREATE TABLE `Requete` (
   `id_requete` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `type_of_validation` SET("ACCES FORMATION", "VALIDATION FORMATION"),
+  `type_of_validation` SET('ACCES FORMATION', 'VALIDATION FORMATION'),
   `id_valideur` int(11) NOT NULL,
   `date_of_validation` date NOT NULL
+);
+
+CREATE TABLE `Formation_Requis` (
+  `id_formation` int(11) NOT NULL,
+  `id_requis` int(11) NOT NULL
 );
 
 ALTER TABLE `Utilisateur` ADD FOREIGN KEY (`manager`) REFERENCES `Utilisateur` (`id_utilisateur`);
@@ -75,6 +79,8 @@ ALTER TABLE `Requete` ADD FOREIGN KEY (`id_valideur`) REFERENCES `Utilisateur` (
 
 ALTER TABLE `Requete` ADD FOREIGN KEY (`id_requete`) REFERENCES `Participe` (`id_participe`);
 
-ALTER TABLE `Formation` ADD FOREIGN KEY (`id_requis`) REFERENCES `Formation` (`id_formation`);
-
 ALTER TABLE `Participe` ADD FOREIGN KEY (`id_formation`) REFERENCES `Formation` (`id_formation`);
+
+ALTER TABLE `Formation_Requis` ADD FOREIGN KEY (`id_formation`) REFERENCES `Formation` (`id_formation`);
+
+ALTER TABLE `Formation_Requis` ADD FOREIGN KEY (`id_requis`) REFERENCES `Formation` (`id_formation`);
