0de959115b09d6b59f4a947f325b89939667bf96
YxZo <v.lopezlopez@student.helmo.be>
2023-03-29 17:17:14 +0200

    debut bd request

--- libs/repository/BdConfig.inc.php
+++ libs/repository/BdConfig.inc.php
@@ -1,6 +1,6 @@
 <?php
 const MYHOST = '192.168.128.13'; //TODO: remplacer par l'adresse IP de votre serveur
-const MYUSER = 'in123456'; //TODO: remplacer par votre numéro d'étudiant
-const MYPASS = '1234';     //TODO : remplacer par votre mot de passe
-const MYDB = 'in123456'; //TODO: remplacer par votre numéro d'étudiant
+const MYUSER = 'in21b10093'; //TODO: remplacer par votre numéro d'étudiant
+const MYPASS = '0093';     //TODO : remplacer par votre mot de passe
+const MYDB = 'in21b10093'; //TODO: remplacer par votre numéro d'étudiant
 ?>
